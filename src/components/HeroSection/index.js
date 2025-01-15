31752b6eb4554cf2fd786c825185b445b0075442
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-01-16 02:59:40 +0530

    commit

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,45 +1,77 @@
-import React from 'react'
-import HeroBgAnimation from '../HeroBgAnimation'
-import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/HeroImage.jpg'
-import Typewriter from 'typewriter-effect';
-import { Bio } from '../../data/constants';
+import React from "react";
+import HeroBgAnimation from "../HeroBgAnimation";
+import BlurText from "./BlurText";
+import {
+  HeroContainer,
+  HeroBg,
+  HeroLeftContainer,
+  Img,
+  HeroRightContainer,
+  HeroInnerContainer,
+  TextLoop,
+  Title,
+  Span,
+  SubTitle,
+  SocialMediaIcons,
+  SocialMediaIcon,
+  ResumeButton,
+} from "./HeroStyle";
+import HeroImg from "../../images/HeroImage.jpg";
+import Typewriter from "typewriter-effect";
+import { Bio } from "../../data/constants";
 
 const HeroSection = () => {
-    return (
-        <div id="about">
-            <HeroContainer>
-                <HeroBg>
-                    <HeroBgAnimation />
-                </HeroBg>
-                <HeroInnerContainer >
-                    <HeroLeftContainer id="Left">
-                        <Title>Hi, I am <br /> {Bio.name}</Title>
-                        <TextLoop>
-                            I am a
-                            <Span>
-                                <Typewriter
-                                    options={{
-                                        strings: Bio.roles,
-                                        autoStart: true,
-                                        loop: true,
-                                    }}
-                                />
-                            </Span>
-                        </TextLoop>
-                        <SubTitle>{Bio.description}</SubTitle>
-                        <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton>
-                    </HeroLeftContainer>
+  const handleAnimationComplete = () => {
+    console.log("Animation completed!");
+  };
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer id="Left">
+            <Title>
+              Hi, I am <br /> {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            {/* <SubTitle>{Bio.description}</SubTitle> */}
+            <BlurText
+              text={Bio.description}
+              delay={50}
+              animateBy="words"
+              direction="top"
+              onAnimationComplete={handleAnimationComplete}
+              //   className="text-2xl mb-8 md:text-center"
+            />
+            <br></br>
+            <ResumeButton href={Bio.resume} target="display">
+              Check Resume
+            </ResumeButton>
+          </HeroLeftContainer>
 
-                    <HeroRightContainer id="Right">
+          <HeroRightContainer id="Right">
+            <Img
+              src={"https://github.com/amanpoddar-dev12.png"}
+              alt="hero-image"
+            />
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
 
-                        <Img src={HeroImg} alt="hero-image" />
-                    </HeroRightContainer>
-                </HeroInnerContainer>
-
-            </HeroContainer>
-        </div>
-    )
-}
-
-export default HeroSection
+export default HeroSection;
